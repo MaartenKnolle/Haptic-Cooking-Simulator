1ff352bb27da8c285f9345c940318491e763bcb5
AlaraEda <alara.ilhan@gmail.com>
2022-11-29 23:42:07 +0100

    Button Part1

---
+++ Assets/Scripts/ButtonVR.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ButtonVR : MonoBehaviour
+{
+    public GameObject button;
+    public UnityEvent onPress;
+    public UnityEvent onRelease;
+    GameObject presser;
+    AudioSource sound;
+    bool isPressed;
+
+    void Start(){
+        sound = GetComponent<AudioSource>();
+        isPressed = false;
+        
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!isPressed){
+            button.transform.localPosition = new Vector3(0, 0.003f, 0);
+            presser = other.gameObject;
+            onPress.Invoke();
+            sound.Play();
+            isPressed = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other){
+        if (isPressed){
+            Debug.Log("Exit the Click2");
+            button.transform.localPosition = new Vector3(0, 0.015f, 0);
+            onRelease.Invoke();
+            isPressed = false;
+        }
+    }
+
+    public void SpawnSphere(){
+        Debug.Log("Spawn???");
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        sphere.transform.localPosition = new Vector3(0,1,2);
+        sphere.AddComponent<Rigidbody>();
+    }
+}
